0fbe30b769fdbce26a4610445f45e5ab8d00a1dc
Soumya Haridas <159426648+SoumyaRithin@users.noreply.github.com>
2024-07-23 17:19:45 +0800

    Add files via upload

---
+++ StringBuffer_Assignment116.java
@@ -0,0 +1,37 @@
+package sele_launch;
+
+
+
+public class StringBuffer_Assignment116 {
+	
+	public static void main(String[] args) {
+		
+	
+	
+	 StringBuffer s1= new StringBuffer("Automation");//mutable
+	 System.out.println(s1);
+	 s1=s1.append(" Testing");
+	 System.out.println(s1);
+	s1.insert(0, "selenium ");
+	
+	 System.out.println(s1);
+	s1.replace(0, 8, "replace");
+	System.out.println(s1);
+	s1.delete(0, 8);
+	System.out.println(s1);
+	s1.reverse();
+	System.out.println(s1);
+	s1.reverse();
+	int a1=s1.capacity();
+	System.out.println(a1);
+	char s2=s1.charAt(8);
+	System.out.println(s2);
+	
+	int a2=s1.length();
+	System.out.println(a2);
+	String s3=s1.substring(11);
+	System.out.println(s3);
+	
+	String s4=s1.substring(0,2);
+	System.out.println(s4);
+}}
