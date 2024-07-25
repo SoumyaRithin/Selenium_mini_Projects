089a4b7a14e4a6724cb2f2bc415159f33a04a709
Soumya Haridas <159426648+SoumyaRithin@users.noreply.github.com>
2024-07-25 17:17:36 +0800

    Add files via upload

---
+++ Threadclass_Assignment118.java
@@ -0,0 +1,17 @@
+package sele_launch;
+
+public class Threadclass_Assignment118 {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		for(int i=10;i>=1;i--)
+		{
+			
+			System.out.println(i);
+			Thread.sleep(1000);
+		}
+
+	}
+
+}
+
